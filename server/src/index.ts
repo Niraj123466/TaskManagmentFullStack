71f16b26a384c9500c385abb6b859e9e88d05b38
devansh <devanshverma193@gmail.com>
2024-09-20 17:27:24 +0530

    prod2

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 
-app.use('/hello',(req, res)=>{
+app.use('/',(req, res)=>{
   res.send("Server is healthy.")
 });
 
